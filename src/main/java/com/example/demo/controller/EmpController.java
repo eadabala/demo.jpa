e4a3c49fb945ef9840c16560fa1762ccc8299207
eadabala <eadabala@gmail.com>
2022-01-30 22:47:25 +0530

    emp controller changes for display all records jira task

--- src/main/java/com/example/demo/controller/EmpController.java
+++ src/main/java/com/example/demo/controller/EmpController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +22,10 @@ public class EmpController {
 	@PostMapping
 	public void createEmp(@RequestBody Emp emp) {
 		empRepo.save(emp);
+	}
+	
+	@GetMapping
+	public List<Emp> getEmpList() {
+		return empRepo.findAll();
 	}
 }
